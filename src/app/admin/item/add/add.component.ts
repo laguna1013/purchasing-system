114652689cdb13ed212bc471908309b450e23b21
laguna99999 <lookatme99999@yahoo.com>
2020-09-16 03:40:17 +1000

    Added item add manual page

--- src/app/admin/item/add/add.component.ts
+++ src/app/admin/item/add/add.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GlobalService } from '../../../services/global.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
-
-  constructor() { }
+  item_id: String = ''
+  item: Object = {
+    
+  };
+  file: File;
+  constructor(public globalService: GlobalService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
-
+  onSelect(event) {
+    const reader = new FileReader();
+    this.file = event.addedFiles[0];
+    reader.readAsDataURL(this.file);
+    reader.onload = () => {
+      this.item['image'] = reader.result;
+    };
+  }
+  remove_item_image = () => {
+    this.item['image'] = '';
+  }
 }
